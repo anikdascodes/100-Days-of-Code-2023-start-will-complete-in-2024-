a02af000ce850e9ec78c64ef687947da1b38d7e7
Anik Das <dasanik01736@gmail.com>
2024-01-07 22:34:02 +0530

    7 jan

--- Day 3/5-exercise_leap_year.py
+++ Day 3/5-exercise_leap_year.py
@@ -6,3 +6,15 @@ This is how we work out whether if a particular year is a leap year
     * except every year that is evenly devisable by 100 with no reminder.
     *unless the year is also devisable by 400 with no reminder.
 '''
+
+if year % 4 ==0:
+    if year % 100:
+        if year % 400:
+            print("Leap Year")
+        else:
+            print("Not Leap Year")
+    else:
+        print("Leap Year")
+
+else:
+    print("Not Leap Year")
