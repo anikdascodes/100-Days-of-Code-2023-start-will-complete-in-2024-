249bf008716bbace2651333c9b221b63ab33a66e
Anik Das <dasanik01736@gmail.com>
2024-03-31 20:48:50 +0530

    1

---
+++ Day 8/3-excercise.py
@@ -0,0 +1,16 @@
+# Write your code below this line 👇
+
+
+
+
+# Write your code above this line 👆
+# Define a function called paint_calc() so the code below works.
+def paint_calc(height, width, cover):
+    number_of_can = (test_h*test_w)/coverage
+    print(f"You'll need {number_of_can} of paint")
+
+# 🚨 Don't change the code below 👇
+test_h = int(input()) # Height of wall (m)
+test_w = int(input()) # Width of wall (m)
+coverage = 5
+paint_calc(round(height=test_h, width=test_w, cover=coverage))
