1483ace8555fb64b14580fbe833e7c1bacc8f88c
Anik Das <dasanik01736@gmail.com>
2024-03-31 17:08:52 +0000

    com

--- Day 8/3-excercise.py
+++ Day 8/3-excercise.py
@@ -1,16 +1,17 @@
 # Write your code below this line 👇
-
+import math
 
 
 
 # Write your code above this line 👆
 # Define a function called paint_calc() so the code below works.
-def paint_calc(height, width, cover):
-    number_of_can = (test_h*test_w)/coverage
-    print(f"You'll need {number_of_can} of paint")
+def paint_calc(height,width,cover):
+    num_cans = (height * width) / cover
+    round_up_cans = math.ceil(num_cans)
+    print(f"You'll need {round_up_cans} cans of paint.")
 
 # 🚨 Don't change the code below 👇
 test_h = int(input()) # Height of wall (m)
 test_w = int(input()) # Width of wall (m)
 coverage = 5
-paint_calc(round(height=test_h, width=test_w, cover=coverage))
+paint_calc(height=test_h, width=test_w, cover=coverage)  
