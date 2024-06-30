9667d1f39ca600568d8360820e9be078223a6985
Anik Das <dasanik01736@gmail.com>
2024-06-30 10:32:39 +0530

    add

---
+++ Day 16/Day 16 recontinue/main.py
@@ -0,0 +1,12 @@
+import tkinter
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+
+my_screen = Screen()
+
+print(my_screen.canvheight)
+my_screen.exitonclick()
